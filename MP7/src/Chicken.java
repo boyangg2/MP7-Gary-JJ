8b9fe6a28f65d295344e6cbb3a714701eea9e0f7
jurgfish <jurgfish@wirelessprv-10-192-42-114.near.illinois.edu>
2017-12-02 16:03:11 -0600

    Implement Fried.

--- MP7/src/Chicken.java
+++ MP7/src/Chicken.java
@@ -1,5 +1,7 @@
 // Gary, JJ
 
 public class Chicken {
-
+	public static void main(String[] args) {
+		System.out.println("Chicken");
+	}
 }
---
+++ MP7/src/Fried.java
@@ -0,0 +1,13 @@
+/**
+ * Specialty Chicken that inherits from Chicken.
+ * 
+ * @author jurgfish
+ */
+public class Fried extends Chicken {
+	public String specialAttack;
+	/* Constructor. */
+	public Fried() {
+		chickType = ChickenType.FRIED;
+	}
+	
+}
