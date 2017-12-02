043d64d38220e6c2b988c34a5b02a4725906f2c9
boyanggong <boyanggong@wirelessprvnat-172-16-183-129.near.illinois.edu>
2017-12-02 16:16:17 -0600

    Test

--- MP7/src/Chicken.java
+++ MP7/src/Chicken.java
@@ -1,7 +1,17 @@
 // Gary, JJ
-
+/** A class to assign the basic characteristics of chicken*/
 public class Chicken {
-	public static void main(String[] args) {
-		System.out.println("Chicken");
+/** we have three different chicken special abilities*/
+	public enum ChickenType {
+		/**Fried Chicken*/
+		FRIED,
+		/**Grilled Chicken*/
+		GRILLED,
+		/**Seasoned Chicken*/
+		SEASONED
 	}
-}
+	/** The type of Chicken we are dealing with*/
+	public ChickenType chickType;
+	
+	
+	}
