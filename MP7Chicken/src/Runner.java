11146f685a6404696f771d451c984dd141ccea17
JJ Urgello <jurgfish@wirelessprv-10-192-42-114.near.illinois.edu>
2017-12-08 14:02:09 -0600

    Update Runner class.

--- MP7Chicken/src/Runner.java
+++ MP7Chicken/src/Runner.java
@@ -1,15 +1,35 @@
 // Gary, JJ 2017
 
+import java.util.Scanner;
+
 public class Runner {
+	static Scanner kb;
+	private static String playerName;
+	private static String potatoName;
 
 	// introduction, ask name
+	public static void intro() {
+		System.out.println("What is your name?");
+		playerName = kb.next().toLowerCase();
+		potatoName = substringPalindrome(playerName);
 
 
+		System.out.println("Your name is " + playerName);
+		System.out.println("The Evil potato's name: " + potatoName);
+	}
 
-	// handle name to create enemy potato name
+	public static String substringPalindrome(String w) {
+		// return the longest substring of w that is a palindrome
+		return w;
+	}
 
 
 
 	// begin real stuff
+
+	public static void main (String[] args) {
+		kb = new Scanner(System.in);
+		intro();
+	}
 
 }
