9cf482123efbb500883edd8a111f662f37aed61c
boyanggong <boyanggong@wirelessprvnat-172-16-249-4.near.illinois.edu>
2017-12-08 17:02:17 -0600

    Finishing setup

--- MP7Chicken/src/Runner.java
+++ MP7Chicken/src/Runner.java
@@ -23,6 +23,44 @@ public class Runner {
 
 		System.out.println("Your name is " + player.getName());
 		System.out.println("The Evil potato's name: " + potato.getName());
+		
+		System.out.println("Now. your health is 100 Js");
+		int health =100; 
+		while(player.getEnergy()>0) {
+			potato.setHealth(health);
+			System.out.println("Your Evil potato's health is"+ health+"Js");
+			System.out.println("Now, the battle begins: What kind of chicken do you wanna use? Type in"
+					+ " (Fried (10J) /Grilled (50J) /Seasoned (100J))");
+			String chickenType = kb.next().toLowerCase(); 
+			int damage;
+			if(chickenType.equals("fried")) {
+				damage=10;
+			}else if(chickenType.equals("grilled")) {
+				damage=50;
+			}else {
+				damage=100;
+			}
+				
+			System.out.println("Do you want to use any weapon bonus? Alert: the weapon bonus may cost you more energy."
+					+ "You make have a risk of dying by using this. Type in (Basic(0)/submachine(10)/Grenade(20)/"
+					+ "Nuclear Rockets(100)");
+			String weaponType = kb.next().toLowerCase();
+			int weaponBonus;
+			if(weaponType.equals("basic")) {
+				weaponBonus=0;
+			}else if(weaponType.equals("submachine")) {
+				weaponBonus=10;
+			}else if(weaponType.equals("grenade")) {
+				weaponBonus=20;
+			}else {
+				weaponBonus=100;
+			}
+			Chicken chick = new Chicken(damage,weaponBonus);
+			}
+		}
+		
+		
+		
 	}
 
 	
